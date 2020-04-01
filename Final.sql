441cff20e43a83dc9261741bf74790643ec9cf91
Edwinrr31 <Edwinrr31@gmail.com>
2020-04-01 00:07:54 -0400

    Database Actualizada

--- Final.sql
+++ Final.sql
@@ -2,23 +2,27 @@ Create database Final
 
 use Final
 
+alter table Clientes
+alter column Telefono float
+
+
 create table Stock (
 Id Int Primary Key,
 Nombre varchar (30) not null,
-Precio int not null, )
+Precio float not null, )
 
 
 create table Facturacion (
 Id Int Primary Key,
-Cedula int not null,
+Cedula float not null,
 Nombre varchar (30) not null,
-Telefono int not null,
+Telefono float not null,
 Email varchar (30) not null, )
 
 create table Clientes (
 Id Int Primary Key,
-Cedula int not null,
+Cedula float not null,
 Nombre varchar (30) not null,
-Telefono int not null,
+Telefono float not null,
 Email varchar (30) not null,
 Categoria varchar (30) not null, )
