b53f6e8314945e68bd78a41cffd8134782899cb3
Edwinrr31 <Edwinrr31@gmail.com>
2020-04-04 02:06:37 -0400

    Consultar datos de la database creada, estos datos seran buscados por linq

--- Final/Controllers/ConsultaController.cs
+++ Final/Controllers/ConsultaController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Final.Models;
 
 namespace Final.Controllers
 {
     public class ConsultaController : Controller
     {
+        private FinalEntities3 db = new FinalEntities3();
         // GET: Consulta
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Filtro()
+        {
+            return View();
+        }
+
+        public ActionResult Consulta(String searching)
+        {
+            var Final = from s in db.Stock
+                        select s;
+
+            if (!String.IsNullOrEmpty(searching))
+            {
+                Final = Final.Where(s => s.Nombre.Contains(searching));
+            }
+            return View(Final.ToList());
         }
     }
 }
